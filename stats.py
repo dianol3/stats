9fb5bbe9d2d2054619115d765a52aaf60079875e
dianol3 <135699030+dianol3@users.noreply.github.com>
2025-10-05 21:58:50 +0100

    Update stats.py

--- stats.py
+++ stats.py
@@ -188,7 +188,7 @@ if st.session_state.page == 1:
         st.session_state.team_name = team_name
         load_players(team_name)
         st.session_state.page = 2
-
+     st.stop()
 # ======================
 # Página 2 - Seleção dos Titulares
 # ======================
@@ -212,7 +212,7 @@ if st.session_state.page == 2:
             st.session_state.page = 3
     else:
         st.warning("O número de jogadores no ficheiro é menor que o número de titulares definido.")
-
+     st.stop()
 # ======================
 # Página 3 - Jogo
 # ======================
