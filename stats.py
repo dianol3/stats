199641c572f5d28e041a5df333bbaaf7bf8db77d
dianol3 <135699030+dianol3@users.noreply.github.com>
2025-10-06 00:35:27 +0100

    Update stats.py

--- stats.py
+++ stats.py
@@ -236,7 +236,7 @@ if st.session_state.page == 'home':
             st.session_state.page = 1
         elif escolha == "Ver Jogos Guardados":
             st.session_state.page = 'ver_jogos'
-st.stop()
+    st.stop()
 # ================
 # ver jogos
 # ===============
